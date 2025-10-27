bb34459cc207801fe4a062015d3cf8c1a74e62cf
HdelossantosRamirez <hdelossantos@dextra.com.do>
2025-10-27 02:39:30 -0400

    Problemas con el java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
@@ -48,7 +48,7 @@ android {
     }
     
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+        kotlinCompilerExtensionVersion = "1.5.17"
     }
     
     packaging {
